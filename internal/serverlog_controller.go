c0f7b00a4e931f443c87711b1ec0d919cd52d0b4
shaojieyue <4yxy1986@gmail.com>
2023-09-10 11:54:00 +0800

    init

--- internal/serverlog_controller.go
+++ internal/serverlog_controller.go
@@ -3,15 +3,12 @@ package internal
 import (
 	"context"
 	"fmt"
-	apiv1 "github.com/yshaojie/log-collector/api/v1"
 	loginformersv1 "github.com/yshaojie/log-collector/pkg/informers/v1"
 	v12 "github.com/yshaojie/log-collector/pkg/listers/v1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
@@ -21,19 +18,6 @@ import (
 const (
 	serverlogDeletionGracePeriod = 1 * time.Minute
 )
-
-var scheme = runtime.NewScheme()
-
-func init() {
-	scheme.AddKnownTypes(apiv1.GroupVersion, &apiv1.ServerLog{})
-
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	scheme.AddKnownTypes(apiv1.GroupVersion, &apiv1.ServerLog{})
-
-	utilruntime.Must(apiv1.AddToScheme(scheme))
-	scheme.AddKnownTypes(apiv1.GroupVersion, &apiv1.ServerLog{})
-	//+kubebuilder:scaffold:scheme
-}
 
 type ServerLogStructController struct {
 	client          clientset.Interface
@@ -51,9 +35,6 @@ func NewServerLogStructController(options Options, informer loginformersv1.Serve
 	}
 	informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-
-			serverLog := obj.(*apiv1.ServerLog)
-			println(serverLog.Name, " ....")
 			controller.enqueue(obj)
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
@@ -69,9 +50,9 @@ func NewServerLogStructController(options Options, informer loginformersv1.Serve
 func (c ServerLogStructController) RUN(ctx context.Context, workers int) {
 	defer utilruntime.HandleCrash()
 	defer c.workqueue.ShutDown()
-	klog.Infof("Starting endpoint controller")
-	defer klog.Infof("Shutting down endpoint controller")
-	if !cache.WaitForNamedCacheSync("", ctx.Done(), c.serverLogSynced) {
+	klog.Infof("Starting log-collector-controller controller")
+	defer klog.Infof("Shutting down log-collector-controller controller")
+	if !cache.WaitForNamedCacheSync("log-collector-controller", ctx.Done(), c.serverLogSynced) {
 		return
 	}
 
@@ -115,7 +96,6 @@ func (c ServerLogStructController) enqueue(obj interface{}) {
 		utilruntime.HandleError(fmt.Errorf("Couldn't get key for object %+v: %v", obj, err))
 		return
 	}
-	println(key)
 	c.workqueue.Add(key)
 }
 
@@ -129,7 +109,6 @@ func (c ServerLogStructController) syncServerLog(key string) error {
 			return err
 		}
 	}
-	println("------->", name)
 	serverLog, err := c.lister.ServerLogs(namespace).Get(name)
 	if serverLog == nil {
 		return nil
